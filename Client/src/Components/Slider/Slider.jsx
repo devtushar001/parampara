942fd551e0b9c91a171d3df37798e6937a6bd467
devtushar001 <tc341269@gmail.com>
2026-01-22 19:02:41 +0530

    Slider Css Updated

--- Client/src/Components/Slider/Slider.jsx
+++ Client/src/Components/Slider/Slider.jsx
@@ -1,13 +1,26 @@
+import { gardenSlider } from '../../assets/assets';
 import './Slider.css';
 const Slider = () => {
     return (
         <>
             <div className="slider">
                 <div className="left">
+                    <div className="arrow-left">
+
+                    </div>
+                    <img src={gardenSlider[0].image} alt="" />
+                    <p>{gardenSlider[0].content}</p>
+                    <div className="arrow-right">
+
+                    </div>
                 </div>
                 <div className="right">
-                    <div className="top"></div>
-                    <div className="bottom"></div>
+                    <div className="top">
+                        <img src={gardenSlider[0].image} alt="" />
+                    </div>
+                    <div className="bottom">
+                        <img src={gardenSlider[0].image} alt="" />
+                    </div>
                 </div>
             </div>
         </>
--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -1,9 +1,23 @@
 import arrowicon from './arrows.png';
 import paramparalogo from './paramparalogo.jpeg';
 import plainlogo from './plainlogop.png';
+import paramparOne from './overview.jpeg';
+import paramparTwo from './overview1.jpeg';
 
 export const icons = {
     arrowicon,
     paramparalogo,
     plainlogo
-}
+};
+
+export const gardenSlider = [
+    {
+        image: paramparOne,
+        content: "Beautifully designed garden venue ideal for weddings and grand celebrations."
+    },
+    {
+        image: paramparTwo,
+        content: "A trusted venue with a proven track record of hosting successful and memorable events."
+    }
+];
+
